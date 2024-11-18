96ad698c12e086f41d65468a9a8556f5ea58289b
ChowNUSISS <81605816+ChowNUSISS@users.noreply.github.com>
2024-11-19 05:32:44 +0800

    Add files via upload

--- src/RN_Project/App.js
+++ src/RN_Project/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import StockDetailsScreen from './screens/StockDetailsScreen';
+import CalendarScreen from './screens/CalendarScreen';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Stock Dashboard' }} />
-        <Stack.Screen name="Details" component={StockDetailsScreen} options={{ title: 'Stock Details' }} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Stock Dashboard' }} />
+        <Tab.Screen name="Details" component={StockDetailsScreen} options={{ title: 'Stock Details' }} />
+        <Tab.Screen name="Calendar" component={CalendarScreen} options={{ title: 'Calendar' }} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
